ccfa13bac5a03e1faebe3848c5675923dca2f128
chuh <chuh2006@outlook.com>
2026-06-30 13:24:27 +0800

    step继续修复编译错误

---
+++ step/fix_write.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference='SilentlyContinue'
+$f = 'C:\Users\33554\Documents\learn\test\ark\step\entry\src\main\ets\pages\ChatPage.ets'
+$lines = Get-Content $f
+$fixed = $lines -join "`r`n"
+[System.IO.File]::WriteAllText($f, $fixed, (New-Object System.Text.UTF8Encoding $true))
+Write-Host "Done. Lines: $($lines.Length)"
